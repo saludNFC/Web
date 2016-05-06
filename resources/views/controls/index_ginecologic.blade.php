29b70f9a18a3b07567d230049ea55791a7253e2e
Veuge <nicky.0592@gmail.com>
2016-05-05 20:14:04 -0400

    New control type

---
+++ resources/views/controls/index_ginecologic.blade.php
@@ -0,0 +1,52 @@
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <div class="col-md-offset-10">
+            {!! link_to_route('paciente.controles.create', 'Crear control', [$patient->id], ['class' => 'btn btn-primary']) !!}
+        </div>
+    </div>
+    <div class="panel-body">
+        @if( !$patient->control->where('control_type', 'Ginecologico')->count() )
+            <div class="alert alert-warning">
+                <h4>Alerta</h4>
+                <p>El paciente {{ $patient->nombre }} {{ $patient->apellido }} no tiene registrados controles ginecologicos</p>
+            </div>
+        @else
+            <table class="table table-stripped table-bordered">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Tipo de control</th>
+                        <th>Fecha ultima menstruacion</th>
+                        <th>Fecha ultima mamografía</th>
+                        <th>Fecha ultimo papanicolau</th>
+                        <th>Encargado</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($patient->control->where('control_type', 'Ginecologico') as $control)
+                    <tr>
+                        <td>{{ $control->id }}</td>
+                        <td>{{ $control->control_type }}</td>
+                        <td>{{ $control->last_menst->format('d-m-Y') }}</td>
+                        <td>
+                            @if($control->last_mamo != null)
+                                {{ $control->last_mamo->format('d-m-Y') }}
+                            @endif
+                        </td>
+                        <td>
+                            @if($control->last_papa != null)
+                                {{ $control->last_papa->format('d-m-Y') }}
+                            @endif
+                        </td>
+                        <td>{{ $control->user->name }}</td>
+                        <td>
+                            @include('controls.partials._actions', $control)
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+    </div>
+</div>
---
+++ resources/views/controls/partials/_ginecologic.blade.php
@@ -0,0 +1,54 @@
+<div class="box box-default">
+    <div class="box-header with-border">
+        <h3 class="box-title">Crear control Ginecológico</h3>
+    </div>
+    <!-- /.box-header -->
+    <!-- form start -->
+    <form></form>
+
+    <div class="form-horizontal">
+        <div class="box-body">
+            {!! Form::hidden('control_type', 'Ginecologico') !!}
+            <div class="form-group{{ $errors->any() ? ($errors->has('last_menst')? ' has-error' : ' has-success') : '' }}">
+                {!! Form::label('last_menst', 'Fecha de ultima menstruación', ['class' => 'col-md-3 control-label']) !!}
+                <div class="col-md-9">
+                    {!! Form::text('last_menst', null, ['class'=>'form-control datepicker', 'placeholder' => 'Fecha de última menstruación', 'id'=>'datepicker']) !!}
+
+                    @if($errors->has('last_menst'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('last_menst') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+            <div class="form-group{{ $errors->any() ? ($errors->has('last_mamo')? ' has-error' : ' has-success') : '' }}">
+                {!! Form::label('last_mamo', 'Fecha de última mamografía', ['class' => 'col-md-3 control-label']) !!}
+                <div class="col-md-9">
+                    {!! Form::text('last_mamo', null, ['class'=>'form-control datepicker', 'placeholder' => 'Fecha de última mamografía', 'id'=>'datepicker']) !!}
+                    @if($errors->has('last_mamo'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('last_mamo') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+            <div class="form-group{{ $errors->any() ? ($errors->has('last_papa')? ' has-error' : ' has-success') : '' }}">
+                {!! Form::label('last_papa', 'Fecha de último papanicolau', ['class' => 'col-md-3 control-label']) !!}
+                <div class="col-md-9">
+                    {!! Form::text('papa', null, ['class'=>'form-control datepicker', 'placeholder' => 'Fecha de último papanicolau', 'id'=>'datepicker']) !!}
+                    @if($errors->has('last_papa'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('last_papa') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+        </div>
+        <div class="box-footer">
+            {!! link_to_route('paciente.show', 'Cancelar', [$patient->id], ['class' => 'btn btn-default']) !!}
+            {!! Form::submit($submit_text, ['class'=>'btn btn-primary']) !!}
+        </div>
+    </div>
+</div>
+
+@include('errors.form_errors')
